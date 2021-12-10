cba1cd1c3bd71abb6284ef66aa4ccb4e791201ac
mathe <mathe@LAPTOP-SV2FEDUE>
2021-12-10 00:54:05 -0300

    Adicionar arquivos de projeto.

---
+++ Aperitif/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Aperitif.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aperitif.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
---
+++ Aperitif/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aperitif.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
